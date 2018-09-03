31b3129ef259c838bd7493ce744f02b489c3cd53
Unknown <bizouerne.m@gmail.com>
2018-09-03 16:54:00 +0200

    ToDoList javascript

---
+++ toDoList.js
@@ -0,0 +1,8 @@
+var myButton = document.getElementById("valeur");
+
+function addToList() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myTextarea").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+}
