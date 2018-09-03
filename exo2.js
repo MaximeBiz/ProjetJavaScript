fdd075e73a6cbf488e6fa3b18f9b36ba8eaba94e
Unknown <marie.garavillon@gmail.com>
2018-09-03 14:41:44 +0200

    Exercice 1

--- exo2.js
+++ exo2.js
@@ -1,6 +1,6 @@
 
 function insertRow() {
-var foo = document.getElementById('myTable').insertRow(0);
+var foo = document.getElementById("myTable").insertRow(0);
 var cell1 = foo.insertCell(0);
 var cell2 = foo.insertCell(1);
 cell1.innerHTML = "NvllLigne Cellule1";
